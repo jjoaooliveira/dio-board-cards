5ab459adda41a88f73a14de076be8303dc86ada8
joao <joaooliv11@hotmail.com>
2025-07-28 21:42:04 -0400

    initial commit

---
+++ src/main/java/com/jjoaooliveira/dio/cardboard/CardboardApplication.java
@@ -0,0 +1,13 @@
+package com.jjoaooliveira.dio.cardboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjoaooliveira/dio/cardboard/CardboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjoaooliveira.dio.cardboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
